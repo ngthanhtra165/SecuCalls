b3c7da44c6f35be1d352986a119822c2a32536cf
seoao24 <seoao24@gmail.com>
2024-04-19 22:41:15 +0700

    commit dashboard chart

---
+++ lib/common/radial_chart.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart' as chart;
+import 'package:syncfusion_flutter_gauges/gauges.dart';
+
+class RadialChart extends StatelessWidget {
+  final String label;
+  final double value;
+  final Color color;
+
+
+  RadialChart({required this.label, required this.value, required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    final _width = MediaQuery.of(context).size.width / 2 - 30;
+    const _valueFontSize = 40.0;
+    const _fontFamily = 'ABeeZee';
+    return Column(
+      children: [
+        const SizedBox(
+          height: 15,
+        ),
+        SizedBox(
+          width: _width,
+          height: 30,
+          child: Text(label, style: TextStyle(
+            fontSize: 25,
+            color: Colors.grey.shade500
+          ), textAlign: TextAlign.center),
+        ),
+        SizedBox(
+          width: _width,
+          height: _width,
+          child: SfRadialGauge(
+            axes: <RadialAxis>[
+              RadialAxis(
+                  showLabels: false,
+                  showTicks: false,
+                  startAngle: 270,
+                  endAngle: 270,
+                  radiusFactor: 0.8,
+                  axisLineStyle: const AxisLineStyle(
+                      thicknessUnit: GaugeSizeUnit.factor, thickness: 0.15),
+                  annotations: <GaugeAnnotation>[
+                    GaugeAnnotation(
+                        angle: 180,
+                        widget: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Text(
+                              value.toInt().toString(),
+                              style: const TextStyle(
+                                  fontFamily: _fontFamily,
+                                  fontSize: _valueFontSize,
+                                  fontWeight: FontWeight.w400,
+                                  fontStyle: FontStyle.italic),
+                            ),
+                            const Text(
+                              '%',
+                              style: TextStyle(
+                                  fontFamily: _fontFamily,
+                                  fontSize: _valueFontSize,
+                                  fontWeight: FontWeight.w400,
+                                  fontStyle: FontStyle.italic),
+                            ),
+                          ],
+                        )),
+                  ],
+                  pointers: <GaugePointer>[
+                    RangePointer(
+                        value: value,
+                        cornerStyle: CornerStyle.bothCurve,
+                        enableAnimation: true,
+                        animationDuration: 1200,
+                        sizeUnit: GaugeSizeUnit.factor,
+                        gradient: SweepGradient(
+                            colors: <Color>[color, color],
+                            stops: const <double>[0.25, 0.75]),
+                        color: const Color(0xFF00A8B5),
+                        width: 0.15),
+                  ]),
+            ],
+          ),
+        )
+      ],
+    );
+
+  }
+
+}
--- lib/screen/dashboard/dashboard_screen.dart
+++ lib/screen/dashboard/dashboard_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:secucalls/common/appbar.dart';
 import 'package:secucalls/common/button.dart';
+import 'package:secucalls/common/radial_chart.dart';
 import 'package:secucalls/common/text_field.dart';
 import 'package:secucalls/constant/design_size.dart';
 import 'package:secucalls/constant/style.dart';
@@ -15,14 +16,17 @@ class DashboardScreen extends StatefulWidget {
   _DashboardScreenState createState() => _DashboardScreenState();
 }
 
-class _DashboardScreenState extends State<DashboardScreen> {
+class _DashboardScreenState extends State<DashboardScreen>
+    with TickerProviderStateMixin {
   @override
   void initState() {
     super.initState();
     number = "3.234.111";
+    _tabController = TabController(length: 3, vsync: this);
   }
 
   late String number;
+  late final TabController _tabController;
 
   @override
   Widget build(BuildContext context) {
@@ -101,9 +105,116 @@ class _DashboardScreenState extends State<DashboardScreen> {
                       ),
                     ],
                   ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TabBar(
+                  controller: _tabController,
+                  labelColor: Colors.black,
+                  indicatorColor: Colors.black,
+                  unselectedLabelColor: Colors.grey.shade500,
+                  padding: EdgeInsets.zero,
+                  tabs: <Widget>[
+                    Tab(
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width / 3,
+                        child: const Text(text_day, textAlign: TextAlign.center, style: TextStyle(
+                          fontSize: 25,
+                            fontWeight: FontWeight.normal,
+                          fontFamily: "ABeeZee"
+                        ),),
+                      ),
+                    ),
+                    Tab(
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width / 3,
+                        child: const Text(text_week, textAlign: TextAlign.center, style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.normal,
+                            fontFamily: "ABeeZee"
+                        ),),
+                      ),
+                    ),
+                    Tab(
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width / 3,
+                        child: const Text(text_month, textAlign: TextAlign.center, style: TextStyle(
+                            fontSize: 25,
+                          fontWeight: FontWeight.normal,
+                            fontFamily: "ABeeZee"
+                        ),),
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  height: 15,
+                  decoration: BoxDecoration(color: Colors.grey.shade200),
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height -
+                      350, // Check lại, trừ đi height của đoạn phía trên tab bar để responsive
+                  child: TabBarView(
+                    controller: _tabController,
+                    children: <Widget>[
+                      DashboardPanel(type: text_day),
+                      DashboardPanel(type: text_week),
+                      DashboardPanel(type: text_month),
+                    ],
+                  ),
                 )
               ],
             )),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _tabController.dispose();
+  }
+}
+
+class DashboardPanel extends StatelessWidget {
+  final String type;
+
+  DashboardPanel({required this.type});
+
+  @override
+  Widget build(BuildContext context) {
+    // mock call api, sau thay bằng giá trị trả về từ api
+    final double _trashNumber = type == text_day ? 75.0 : type == text_week ? 64.0 : 32.0;
+    final double _cheatNumber = type == text_day ? 25.0 : type == text_week ? 30.0 : 17.0;
+    final double _adsNumber = type == text_day ? 50.0 : type == text_week ? 37.0 : 22.0;
+    final double _totalNumber = type == text_day ? 75.0 : type == text_week ? 80.0 : 66.0;
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              RadialChart(
+                  label: "Số rác",
+                  value: _trashNumber,
+                  color: Colors.deepOrange.shade300),
+              RadialChart(
+                  label: "Số lừa đảo", value: _cheatNumber, color: Colors.orangeAccent)
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              RadialChart(
+                  label: "Số quảng cáo", value: _adsNumber, color: Colors.pinkAccent),
+              RadialChart(label: "Tổng số", value: _totalNumber, color: Colors.green)
+            ],
+          )
+        ],
       ),
     );
   }
